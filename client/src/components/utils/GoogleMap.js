56e6018d5c0d8a269fdf6f5eeb16d472d35590dd
Florian Lahitte <lahitte.florian@gmail.com>
2020-09-22 13:55:25 -0400

    new google maps

--- client/src/components/utils/GoogleMap.js
+++ client/src/components/utils/GoogleMap.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div><i style={{color: 'red', fontSize: '40px'}}className="fas fa-map-marker-alt"></i></div>;
- 
+
+const AnyReactComponent = ({ text }) => (
+  <div>
+    <i
+      style={{ color: 'red', fontSize: '40px' }}
+      className="fas fa-map-marker-alt"
+    ></i>
+  </div>
+);
+
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
       lat: 44.774221,
-      lng: -0.613080
+      lng: -0.61308,
     },
-    zoom: 17
+    zoom: 17,
   };
- 
+
   render() {
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '300px', width: '100%', marginBottom: '60px' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCeAipRGvfEcH30zU8l2XMdvrtycHpV55g' }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={44.774221}
-            lng={-0.613080}
-            text="My Marker"
-          />
+          <AnyReactComponent lat={44.774221} lng={-0.61308} text="My Marker" />
         </GoogleMapReact>
       </div>
     );
   }
 }
- 
+
 export default SimpleMap;
